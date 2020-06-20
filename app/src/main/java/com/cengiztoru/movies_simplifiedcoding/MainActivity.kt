c56379d310eb2ae9306ed5cf3143508120b52a7e
Cengiz TORU <cengiztoru@gmail.com>
2020-06-21 00:43:35 +0300

    First Commit with ❤

---
+++ app/src/main/java/com/cengiztoru/movies_simplifiedcoding/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.cengiztoru.movies_simplifiedcoding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+//Tutorial => https://www.youtube.com/watch?v=mNDN9YEcPiE&list=PLk7v1Z2rk4hjtIT9TCKIcl2YJYfDlZ_4v
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
